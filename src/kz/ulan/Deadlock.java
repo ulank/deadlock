757c5153d87560f9e5b8aaf9b32207949802091f
Ulan <kozhabekov.u@gmail.com>
2023-01-16 17:37:18 +0600

    Deadlock changes

--- src/kz/ulan/Deadlock.java
+++ src/kz/ulan/Deadlock.java
@@ -1,6 +1,46 @@
-package kz.ulan;/**
+package kz.ulan;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
  * Created by Ulan on 1/16/2023
  */
- 
- public class Deadlock {
+
+public class Deadlock {
+
+    public static void main(String[] args) {
+        var a = new Object();
+        var b = new Object();
+
+        var barrier = new CyclicBarrier(2);
+
+        new Thread(() -> {
+            synchronized (a) {
+                System.out.println("Catch A");
+                try {
+                    barrier.await(); // sync
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    throw new RuntimeException(e);
+                }
+                synchronized (b) {
+                    System.out.println("Now catch B");
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            synchronized (b) {
+                System.out.println("Catch B");
+                try {
+                    barrier.await(); // sync
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    throw new RuntimeException(e);
+                }
+                synchronized (a) {
+                    System.out.println("Now catch A");
+                }
+            }
+        }).start();
+    }
 }
